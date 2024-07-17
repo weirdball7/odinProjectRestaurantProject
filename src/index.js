9f67ce1b598810ffece78fef7981821efa7570f2
weirdball7 <moteamitay04@gmail.com>
2024-07-17 21:12:02 +0300

    add the home tab module not working havent figuerd out why yet

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import "./style.css";
 import './initPageLoad';    
+import './homeTab';
 import { initialPageLoad } from "./initPageLoad";
+import { homeBtn } from "./initPageLoad";
+import { menuBtn } from "./initPageLoad";
+import { aboutBtn } from "./initPageLoad";
+import { contactBtn } from "./initPageLoad";
+import { loadHomeTab } from "./homeTab";
+
+
 
 console.log("hello");
 // const div = document.createElement("div");
 // div.setAttribute("id", "content");
 initialPageLoad();
+
+homeBtn.addEventListener('click', loadHomeTab);
+
